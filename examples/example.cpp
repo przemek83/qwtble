4dcb05af74024b44048d6c865184fd8735a384e7
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-03-16 19:42:08 +0100

    Example quantiles plot
    Added quantiles plot into examples.

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,7 +1,34 @@
 #include <QApplication>
+#include <QGroupBox>
 #include <QStyleFactory>
 #include <QWidget>
 #include <QVBoxLayout>
+
+#include <BasicDataPlot.h>
+#include <GroupPlot.h>
+#include <HistogramPlot.h>
+#include <PlotBase.h>
+#include <QuantilesPlot.h>
+
+static QGroupBox* wrapPlot(const QString& name,
+                           PlotBase* plot)
+{
+    auto groupBox = new QGroupBox(name);
+    auto layout = new QVBoxLayout();
+    layout->addWidget(plot);
+    groupBox->setLayout(layout);
+    return groupBox;
+}
+
+static QuantilesPlot* createQuantilesPlot()
+{
+    auto quantilesPlot = new QuantilesPlot();
+    Quantiles quantiles;
+    QVector<float> exampleValues {3.5F, 6.7F, 4.7F, 6.6F, 3.F, 4.9F};
+    quantiles.computeQuantiles(exampleValues);
+    quantilesPlot->setNewData(quantiles);
+    return quantilesPlot;
+}
 
 int main(int argc, char* argv[])
 {
@@ -11,8 +38,10 @@ int main(int argc, char* argv[])
 
     QWidget widget;
     QHBoxLayout widgetLayout(&widget);
+    widgetLayout.addWidget(wrapPlot("Quantiles plot", createQuantilesPlot()));
     widget.setLayout(&widgetLayout);
     widget.show();
+    widget.resize(250, 500);
 
     return QApplication::exec();
 }
