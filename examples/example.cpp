ca4af1805705e3e6d8e36e82bf66fe4bb1ac7847
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-03-17 19:08:05 +0100

    Update example.cpp
    Cleaning, final improvements.

--- examples/example.cpp
+++ examples/example.cpp
@@ -22,33 +22,35 @@ static QVector<QString> exampleNames {"first", "second", "third", "fourth", "fif
 static const QVector<float> examplePriceSeries
 {
     84.66F, 40.F, 5.F, 7.02F, 177.01F, 110.23F, 138.12F, 54.94F, 20.F, 80.F,
-    15.85F, 26.77F, 51.61F, 144.44F, 106.F, 8.06F, 92.5F, 19.57F, 91.44F,
-    90.F, 75.F, 39.56F, 0.2F, 25.F, 80.F, 54.86F, 2.13F, 4.84F, 15.F,
-    2.67F, 95.F, 100.F, 5.F, 5.F, 96.38F, 1.99F, 75.08F, 93.37F, 99.82F,
-    94.81F, 2.35F, 140.44F, 150.F, 138.72F, 295.25F, 50.F, 110.97F, 4.87F,
-    45.F, 60.F, 90.F, 66.17F, 78.68F, 75.87F, 8.95F, 65.F, 1.3F, 5.83F,
-    44.13F, 59.65F, 102.52F, 108.45F, 70.F, 70.F, 70.F, 70.F, 68.F, 7.92F,
-    36.34F, 30.3F, 93.69F, 146.81F, 79.F, 100.F, 70.F, 61.79F, 38.42F,
-    19.41F, 45.45F, 7.39F, 223.55F, 45.F, 90.F, 90.F, 75.21F, 45.F, 130.F,
-    17.44F, 75.F, 69.9F, 112.74F, 126.36F, 40.F, 30.F, 1.88F, 46.2F, 2.99F,
-    48.69F, 21.74F, 98.39F, 65.F, 147.98F, 25.F, 45.F, 5.38F
+    15.85F, 26.77F, 51.61F, 144.44F, 106.F, 8.06F, 92.5F, 19.57F, 91.44F, 90.F,
+    75.F, 39.56F, 0.2F, 25.F, 80.F, 54.86F, 2.13F, 4.84F, 15.F, 2.67F, 95.F,
+    100.F, 5.F, 5.F, 96.38F, 1.99F, 75.08F, 93.37F, 99.82F, 94.81F, 2.35F,
+    140.44F, 150.F, 138.72F, 295.25F, 50.F, 110.97F, 4.87F, 45.F, 60.F, 90.F,
+    66.17F, 78.68F, 75.87F, 8.95F, 65.F, 1.3F, 5.83F, 44.13F, 59.65F, 102.52F,
+    108.45F, 70.F, 70.F, 70.F, 70.F, 68.F, 7.92F, 36.34F, 30.3F, 93.69F,
+    146.81F, 79.F, 100.F, 70.F, 61.79F, 38.42F, 19.41F, 45.45F, 7.39F, 223.55F,
+    45.F, 90.F, 90.F, 75.21F, 45.F, 130.F, 17.44F, 75.F, 69.9F, 112.74F,
+    126.36F, 40.F, 30.F, 1.88F, 46.2F, 2.99F, 48.69F, 21.74F, 98.39F, 65.F,
+    147.98F, 25.F, 45.F, 5.38F
 };
 
 // As number of days since 1.1.1970.
-static const QVector<double> exampleDateSeries
+static const QVector<float> exampleDateSeries
 {
-    13959., 13941., 13984., 13914., 13999., 13979., 13945., 13999., 14013.,
-    13972., 13992., 13969., 13951., 13970., 13951., 13970., 13986., 13993.,
-    14011., 13969., 13977., 14026., 13990., 13916., 13972., 13999., 13980.,
-    13956., 13977., 14019., 14028., 13999., 13950., 13950., 14011., 14011.,
-    14026., 13965., 14032., 14008., 13951., 14012., 13991., 14007., 14019.,
-    13980., 13997., 14042., 13985., 13991., 13986., 13972., 13965., 13949.,
-    13990., 14035., 14008., 14019., 13964., 13957., 14033., 14014., 13980.,
-    13980., 13980., 13980., 13980., 13992., 13999., 13943., 13997., 13920.,
-    13977., 13944., 13986., 13956., 13966., 13951., 14032., 14027., 13993.,
-    13980., 13986., 13986., 13983., 13955., 13941., 14013., 14042., 14039.,
-    14042., 13937., 13914., 13915., 13930., 13845., 13864., 13857., 13867.,
-    13929., 13972., 13910., 13924., 13924., 13927.};
+    13959.F, 13941.F, 13984.F, 13914.F, 13999.F, 13979.F, 13945.F, 13999.F,
+    14013.F, 13972.F, 13992.F, 13969.F, 13951.F, 13970.F, 13951.F, 13970.F,
+    13986.F, 13993.F, 14011.F, 13969.F, 13977.F, 14026.F, 13990.F, 13916.F,
+    13972.F, 13999.F, 13980.F, 13956.F, 13977.F, 14019.F, 14028.F, 13999.F,
+    13950.F, 13950.F, 14011.F, 14011.F, 14026.F, 13965.F, 14032.F, 14008.F,
+    13951.F, 14012.F, 13991.F, 14007.F, 14019.F, 13980.F, 13997.F, 14042.F,
+    13985.F, 13991.F, 13986.F, 13972.F, 13965.F, 13949.F, 13990.F, 14035.F,
+    14008.F, 14019.F, 13964.F, 13957.F, 14033.F, 14014.F, 13980.F, 13980.F,
+    13980.F, 13980.F, 13980.F, 13992.F, 13999.F, 13943.F, 13997.F, 13920.F,
+    13977.F, 13944.F, 13986.F, 13956.F, 13966.F, 13951.F, 14032.F, 14027.F,
+    13993.F, 13980.F, 13986.F, 13986.F, 13983.F, 13955.F, 13941.F, 14013.F,
+    14042.F, 14039.F, 14042.F, 13937.F, 13914.F, 13915.F, 13930.F, 13845.F,
+    13864.F, 13857.F, 13867.F, 13929.F, 13972.F, 13910.F, 13924.F, 13924.F,
+    13927.F};
 
 static QGroupBox* wrapPlot(const QString& name,
                            PlotBase* plot)
@@ -65,7 +67,7 @@ static QuantilesPlot* createQuantilesPlot()
     auto quantilesPlot = new QuantilesPlot();
     Quantiles quantiles;
     quantiles.computeQuantiles(exampleValues.first());
-    quantilesPlot->setAxisScale(QwtPlot::xBottom, 0, 2, 0);
+    quantilesPlot->setAxisScale(QwtPlot::xBottom, 0, 2);
     quantilesPlot->setNewData(quantiles);
     return quantilesPlot;
 }
@@ -87,6 +89,15 @@ static GroupPlot* createGroupPlot()
     return groupPlot;
 }
 
+static double* getRawData(QVector<float> data)
+{
+    int numberOfItems {data.size()};
+    double* rawData = new double[static_cast<unsigned long long>(numberOfItems)];
+    for (int i = 0; i < examplePriceSeries.size();  ++i)
+        rawData[i] = static_cast<double>(data[i]);
+    return rawData;
+}
+
 static HistogramPlot* createHistogramPlot()
 {
     Quantiles quantiles;
@@ -94,11 +105,9 @@ static HistogramPlot* createHistogramPlot()
     quantiles.computeQuantiles(dataForQuantiles);
     auto histogramPlot = new HistogramPlot();
     const int numberOfItems {examplePriceSeries.size()};
-    double* dataX = new double[static_cast<unsigned long long>(numberOfItems)];
-    double* dataY = new double[static_cast<unsigned long long>(numberOfItems)];
-    for (int i = 0; i < examplePriceSeries.size();  ++i)
-        dataY[i] = static_cast<double>(examplePriceSeries[i]);
-    PlotData plotData(dataX, dataY, numberOfItems);
+    PlotData plotData(getRawData(exampleDateSeries),
+                      getRawData(examplePriceSeries),
+                      numberOfItems);
     histogramPlot->setNewData(plotData, quantiles, 10);
     return histogramPlot;
 }
@@ -109,19 +118,25 @@ static BasicDataPlot* createBasicDataPlot()
     QVector<float> dataForQuantiles(examplePriceSeries);
     quantiles.computeQuantiles(dataForQuantiles);
     auto basicDataPlot = new BasicDataPlot();
+    const auto [min, max] =
+        std::minmax_element(exampleDateSeries.begin(), exampleDateSeries.end());
+    quantiles.minX_ = *min;
+    quantiles.maxX_ = *max;
+    basicDataPlot->setAxisScale(QwtPlot::xBottom,
+                                static_cast<double>(*min),
+                                static_cast<double>(*max));
+    const QVector<QPointF> linearRegressionPoints
+    {
+        {static_cast<double>(*min), 38.002},
+        {static_cast<double>(*max), 78.4491}
+    };
     const int numberOfItems {examplePriceSeries.size()};
-    double* dataX = new double[static_cast<unsigned long long>(numberOfItems)];
-    for (int i = 0; i < exampleDateSeries.size(); ++i)
-        dataX[i] = exampleDateSeries[i];
-    double* dataY = new double[static_cast<unsigned long long>(numberOfItems)];
-    for (int i = 0; i < examplePriceSeries.size(); ++i)
-        dataY[i] = static_cast<double>(examplePriceSeries[i]);
-    const auto [min, max] = std::minmax_element(exampleDateSeries.begin(), exampleDateSeries.end());
-    quantiles.minX_ = static_cast<float>(*min);
-    quantiles.maxX_ = static_cast<float>(*max);
-    basicDataPlot->setAxisScale(QwtPlot::xBottom, *min, *max);
-    PlotData plotData(dataX, dataY, numberOfItems);
-    basicDataPlot->setNewData(plotData, quantiles, {{13845, 38.002}, {14042, 78.4491}});
+    PlotData plotData(getRawData(exampleDateSeries),
+                      getRawData(examplePriceSeries),
+                      numberOfItems);
+    basicDataPlot->setNewData(plotData,
+                              quantiles,
+                              linearRegressionPoints);
     return basicDataPlot;
 }
 
@@ -133,16 +148,19 @@ int main(int argc, char* argv[])
 
     QWidget widget;
     QVBoxLayout widgetLayout(&widget);
+
     QSplitter* upperSplitter = new QSplitter(&widget);
     upperSplitter->addWidget(wrapPlot("Quantiles plot", createQuantilesPlot()));
     upperSplitter->addWidget(wrapPlot("Histogram plot", createHistogramPlot()));
     upperSplitter->setSizes({200, 600});
     widgetLayout.addWidget(upperSplitter);
+
     QSplitter* lowerSplitter = new QSplitter(&widget);
     lowerSplitter->addWidget(wrapPlot("Grouping plot", createGroupPlot()));
     lowerSplitter->addWidget(wrapPlot("Basic data plot", createBasicDataPlot()));
     lowerSplitter->setSizes({300, 500});
     widgetLayout.addWidget(lowerSplitter);
+
     widget.setLayout(&widgetLayout);
     widget.show();
     widget.resize(800, 600);
