0b46c44131442d5aa3074e60b362a108bda6fefd
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-03-17 12:14:44 +0100

    Added grouping plot to examples
    Added grouping plot to examples. Small rework.

--- examples/example.cpp
+++ examples/example.cpp
@@ -10,6 +10,11 @@
 #include <PlotBase.h>
 #include <QuantilesPlot.h>
 
+static QVector<QVector<float>> exampleValues {{3.5F, 6.7F, 4.7F, 6.6F, 3.F, 4.9F},
+    {5.5F, 1.2F, 3.2F, 7.6F, 5.F, 4.9F, 0.3F, 7.3F},
+    {2.1F, 1.7F, 4.3F, 6.6F, 1.F, 3.9F, 5.5F}};
+static QVector<QString> exampleNames {"first", "second", "third"};
+
 static QGroupBox* wrapPlot(const QString& name,
                            PlotBase* plot)
 {
@@ -24,10 +29,26 @@ static QuantilesPlot* createQuantilesPlot()
 {
     auto quantilesPlot = new QuantilesPlot();
     Quantiles quantiles;
-    QVector<float> exampleValues {3.5F, 6.7F, 4.7F, 6.6F, 3.F, 4.9F};
-    quantiles.computeQuantiles(exampleValues);
+    quantiles.computeQuantiles(exampleValues.first());
     quantilesPlot->setNewData(quantiles);
     return quantilesPlot;
+}
+
+static GroupPlot* createGroupPlot()
+{
+    QVector<Quantiles> quantilesVector;
+    for (auto& values : exampleValues)
+    {
+        Quantiles quantiles;
+        quantiles.computeQuantiles(values);
+        quantilesVector.push_back(quantiles);
+    }
+    auto groupPlot = new GroupPlot();
+    groupPlot->setAxisScale(QwtPlot::yLeft, 0, 8);
+    groupPlot->setAxisScale(QwtPlot::yRight, 0, 8);
+    groupPlot->setAxisScale(QwtPlot::xBottom, 0, exampleValues.size() + 1, 1);
+    groupPlot->setNewData(quantilesVector, exampleNames);
+    return groupPlot;
 }
 
 int main(int argc, char* argv[])
@@ -39,9 +60,10 @@ int main(int argc, char* argv[])
     QWidget widget;
     QHBoxLayout widgetLayout(&widget);
     widgetLayout.addWidget(wrapPlot("Quantiles plot", createQuantilesPlot()));
+    widgetLayout.addWidget(wrapPlot("Grouping plot", createGroupPlot()));
     widget.setLayout(&widgetLayout);
     widget.show();
-    widget.resize(250, 500);
+    widget.resize(500, 500);
 
     return QApplication::exec();
 }
