bfaeef6e53e1351dfc92118268d6b71b71f67d13
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-11-21 13:57:48 +0100

    Build only needed functionalities from Qwt library.

---
+++ qwt_preconfigure.cmake
@@ -0,0 +1,30 @@
+
+cmake_minimum_required(VERSION 3.16)
+
+# Rewrite qwtconfig.pri inplace dropping commented lines and  commenting all 
+# lines having one of given keywords. Compile only neded by project files.
+function(CONFIGURE_QWT)
+    file(STRINGS "${CMAKE_BINARY_DIR}/qwtconfig.pri" inputLines)
+
+    set(keywords "QwtExamples" "QwtPlayground" "QwtTests" "QwtDesignerSelfContained" "QwtDesigner" "QwtPolar" "QwtSvg")
+    set(modifiedLines)
+    foreach(line IN LISTS inputLines)
+        if("${line}" MATCHES "^[ \t]*#")
+            continue() 
+        endif()
+
+        foreach(keyword IN LISTS keywords) 
+            if("${line}" MATCHES ".*${keyword}.*") 
+                set(line "# ${line}")
+                break() 
+            endif() 
+        endforeach()
+
+        list(APPEND modifiedLines "${line}")
+    endforeach()
+
+    string(REPLACE ";" "\n" outputContent "${modifiedLines}")
+    file(WRITE "${CMAKE_BINARY_DIR}/qwtconfig.pri" "${outputContent}")
+endfunction()
+
+configure_qwt()
