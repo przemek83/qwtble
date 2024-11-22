f8abfb748708329902130356b695161aa5689fb5
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-11-22 10:47:27 +0100

    Some ranamings in CMake files.

--- qwt_setup.cmake
+++ qwt_setup.cmake
@@ -1,7 +1,7 @@
 
 cmake_minimum_required(VERSION 3.16)
 
-function(INSTALL_QWT)
+function(QWT_SETUP)
     if(MSVC)
         set(QWT_BUILD_COMMAND "nmake")
     else()
@@ -15,7 +15,7 @@ function(INSTALL_QWT)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/qwt/install)
     file(COPY ${CMAKE_BINARY_DIR}/qwt/qwt/src/qwt-build/lib DESTINATION ${CMAKE_BINARY_DIR}/qwt/install)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/qwt/install/include)
-    file(GLOB QWT_HEADERS "${CMAKE_BINARY_DIR}/qwt/qwt/src/qwt/src/*.h" )
+    file(GLOB QWT_HEADERS "${CMAKE_BINARY_DIR}/qwt/qwt/src/qwt/src/*.h")
     file(COPY ${QWT_HEADERS} DESTINATION ${CMAKE_BINARY_DIR}/qwt/install/include)
     set(QWT_INCLUDE_PATH ${CMAKE_BINARY_DIR}/qwt/install/include)
     set(QWT_BINARY_PATH ${CMAKE_BINARY_DIR}/qwt/install/lib)
@@ -27,4 +27,4 @@ function(INSTALL_QWT)
     link_directories(${QWT_BINARY_PATH})
 endfunction()
 
-install_qwt()
+qwt_setup()
