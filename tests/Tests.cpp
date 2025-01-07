99677f455250437401f103fdf0f8d987632e87a2
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-01-07 15:31:54 +0100

    Add light palette setup and apply Fusion style in test application

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,12 +1,50 @@
+#include <QStyleFactory>
 #include <QTest>
 
+#include "BasicDataPlotTest.h"
 #include "GroupPlotUITest.h"
 #include "QuantilesTest.h"
 #include "UtilitiesTest.h"
 
+namespace
+{
+void setLightPalette()
+{
+    QPalette palette;
+    const QColor color(240, 240, 240);
+    const QColor altColor(225, 225, 225);
+    palette.setColor(QPalette::Window, color);
+    palette.setColor(QPalette::Button, color);
+    palette.setColor(QPalette::Window, color);
+    palette.setColor(QPalette::Dark, altColor);
+    palette.setColor(QPalette::Light, altColor);
+    palette.setColor(QPalette::Midlight, altColor);
+    palette.setColor(QPalette::Mid, altColor);
+    palette.setColor(QPalette::Shadow, altColor);
+    palette.setColor(QPalette::WindowText, Qt::black);
+    palette.setColor(QPalette::Base, Qt::white);
+    palette.setColor(QPalette::AlternateBase, altColor);
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::black);
+    palette.setColor(QPalette::Text, Qt::black);
+    palette.setColor(QPalette::Button, color);
+    palette.setColor(QPalette::ButtonText, Qt::black);
+    palette.setColor(QPalette::BrightText, Qt::red);
+    palette.setColor(QPalette::Link, altColor);
+    palette.setColor(QPalette::Highlight, altColor);
+    palette.setColor(QPalette::HighlightedText, Qt::white);
+
+    QApplication::setPalette(palette);
+}
+}  // namespace
+
 int main(int argc, char* argv[])
 {
     const QApplication a(argc, argv);
+
+    QStyle* style = QStyleFactory::create(QStringLiteral("Fusion"));
+    QApplication::setStyle(style);
+    setLightPalette();
 
     QLocale locale(QLocale::English, QLocale::UnitedStates);
     QLocale::setDefault(locale);
