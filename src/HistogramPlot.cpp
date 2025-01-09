e18c9779887f01ba02ac1a1409382e32a48fddcf
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-01-09 13:14:50 +0100

    Refactor HistogramPlot to use modern C++ features and improve code clarity.

--- src/HistogramPlot.cpp
+++ src/HistogramPlot.cpp
@@ -10,7 +10,7 @@
 
 HistogramPlot::HistogramPlot(QWidget* parent)
     : PlotBase(QObject::tr("Histogram"), parent),
-      picker_(new XYAxisNumberPicker(canvas()))
+      picker_(std::make_unique<XYAxisNumberPicker>(canvas()))
 {
     initHistogramPlot();
     initActualDensity();
@@ -34,7 +34,7 @@ void HistogramPlot::initActualDensity()
     distributionCurve_.setTitle(QObject::tr("Distribution"));
     distributionCurve_.setStyle(QwtPlotCurve::Lines);
     distributionCurve_.setCurveAttribute(QwtPlotCurve::Fitted);
-    QPen pen = distributionCurve_.pen();
+    QPen pen{distributionCurve_.pen()};
     pen.setColor(QColor(Qt::blue));
     pen.setWidth(2);
     distributionCurve_.setPen(pen);
@@ -45,7 +45,7 @@ void HistogramPlot::initActualDensity()
 
 void HistogramPlot::initLegend()
 {
-    auto* legend = new QwtLegend();
+    auto* legend{new QwtLegend()};
     legend->setDefaultItemMode(QwtLegendData::Checkable);
     legend->setFrameStyle(QFrame::Box | QFrame::Sunken);
 
@@ -54,14 +54,14 @@ void HistogramPlot::initLegend()
             SLOT(legendItemChecked(QVariant, bool, int)));
     insertLegend(legend, QwtPlot::BottomLegend);
 
-    setLegendItemChecked(itemToInfo(&histogram_));
-    setLegendItemChecked(itemToInfo(&distributionCurve_));
+    setLegendItemChecked(QwtPlot::itemToInfo(&histogram_));
+    setLegendItemChecked(QwtPlot::itemToInfo(&distributionCurve_));
 }
 
 void HistogramPlot::legendItemChecked(const QVariant& itemInfo, bool on,
                                       [[maybe_unused]] int index)
 {
-    QwtPlotItem* plotItem = infoToItem(itemInfo);
+    QwtPlotItem* plotItem{infoToItem(itemInfo)};
     if (plotItem != nullptr)
     {
         plotItem->setVisible(on);
@@ -85,20 +85,21 @@ QVector<int> HistogramPlot::getFilledIntervals(const QVector<double>& data,
                                                const Quantiles& quantiles,
                                                int intervalsCount)
 {
-    const int dataCount = data.size();
-    const double min = quantiles.min_;
-    const double max = quantiles.max_;
+    const qsizetype dataCount{data.size()};
+    const double min{quantiles.min_};
+    const double max{quantiles.max_};
     const double stepSize = (max - min) / intervalsCount;
 
-    QVector<int> intervals(std::max(intervalsCount, dataCount));
+    QVector<int> intervals(
+        std::max(static_cast<qsizetype>(intervalsCount), dataCount));
     for (int i = 0; i < dataCount; ++i)
     {
-        int index = static_cast<int>((data[i] - min) / stepSize);
-        if (index > dataCount - 1)
+        qsizetype index{static_cast<qsizetype>((data[i] - min) / stepSize)};
+        if (index > (dataCount - 1))
             index = dataCount - 1;
         if (index < 0)
             index = 0;
-        intervals[index]++;
+        ++intervals[index];
     }
     return intervals;
 }
@@ -107,16 +108,16 @@ void HistogramPlot::udpatePlotItems(int intervalsCount)
 {
     QVector<int> intervals{
         getFilledIntervals(data_, quantiles_, intervalsCount)};
-    const double min = quantiles_.min_;
-    const double max = quantiles_.max_;
-    const double stepSize = (max - min) / intervalsCount;
+    const double min{quantiles_.min_};
+    const double max{quantiles_.max_};
+    const double stepSize{(max - min) / intervalsCount};
     const double middleOfStep{stepSize / 2.};
 
     QVector<QwtIntervalSample> samples;
     QVector<QPointF> actualPoints;
     for (int i = 0; i < intervalsCount; ++i)
     {
-        const double from = min + stepSize * i;
+        const double from{min + (stepSize * i)};
         samples.append(QwtIntervalSample(intervals[i], from, from + stepSize));
         actualPoints.append(QPointF(from + middleOfStep, intervals[i]));
     }
